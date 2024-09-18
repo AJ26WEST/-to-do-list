d8d2ec7e1df4fb2582900d55c757c10af2e6ba02
Abhishek joji <148741327+AJ26WEST@users.noreply.github.com>
2024-09-18 11:08:12 +0530

    Create Create the To-Do List Class.java

---
+++ Create the To-Do List Class.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToDoList {
+    private ArrayList<String> tasks;
+
+    public ToDoList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+        System.out.println("Task added: " + task);
+    }
+
+    public void viewTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks in the list.");
+        } else {
+            System.out.println("To-Do List:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+
+    public void removeTask(int index) {
+        if (index < 1 || index > tasks.size()) {
+            System.out.println("Invalid task number.");
+        } else {
+            System.out.println("Removing task: " + tasks.remove(index - 1));
+        }
+    }
+}
